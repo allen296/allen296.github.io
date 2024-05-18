15734c912b077207a0cc945ab7367f9dbd47801a
Antonio Valladares García <33860248+allen296@users.noreply.github.com>
2024-05-18 03:30:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+let audio = new Audio('NeonNights.mp3');
+let isPlaying = false;
+
 document.body.addEventListener('click', function() {
-    let audio = new Audio('NeonNights.mp3');
-    audio.play();
-    // Desvincular el evento de clic después de la primera interacción para evitar múltiples reproducciones
-    document.body.removeEventListener('click', arguments.callee);
+    if (!isPlaying) {
+        audio.loop = true; // Reproducir en bucle
+        audio.play();
+        isPlaying = true;
+    }
 });
