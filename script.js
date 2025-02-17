002f10ba1b340df4df237cb6fda22c83399966aa
Antonio Valladares García <33860248+allen296@users.noreply.github.com>
2025-02-17 11:03:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,39 +3,43 @@ const track = document.getElementById("image-track");
 const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
 
 const handleOnUp = () => {
-    track.dataset.mouseDownAt = "0";  
-    track.dataset.prevPercentage = track.dataset.percentage;
+  track.dataset.mouseDownAt = "0";  
+  track.dataset.prevPercentage = track.dataset.percentage;
 }
 
 const handleOnMove = e => {
-    if (track.dataset.mouseDownAt === "0") return;
+  if(track.dataset.mouseDownAt === "0") return;
   
-    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
-          maxDelta = window.innerWidth / 2;
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+        maxDelta = window.innerWidth / 2;
   
-    const percentage = (mouseDelta / maxDelta) * -100,
-          nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
-          nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+  const percentage = (mouseDelta / maxDelta) * -100,
+        nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
+        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
   
-    track.dataset.percentage = nextPercentage;
+  track.dataset.percentage = nextPercentage;
   
-    track.animate({
-        transform: `translate(${nextPercentage}%, -50%)`
+  track.animate({
+    transform: `translate(${nextPercentage}%, -50%)`
+  }, { duration: 1200, fill: "forwards" });
+  
+  for(const image of track.getElementsByClassName("image")) {
+    image.animate({
+      objectPosition: `${100 + nextPercentage}% center`
     }, { duration: 1200, fill: "forwards" });
-  
-    for (const image of track.getElementsByClassName("image")) {
-        image.animate({
-            objectPosition: `${100 + nextPercentage}% center`
-        }, { duration: 1200, fill: "forwards" });
-    }
+  }
 }
 
-/* Manejo de eventos para mouse y pantallas táctiles */
+/* -- Had to add extra lines for touch events -- */
+
 window.onmousedown = e => handleOnDown(e);
+
 window.ontouchstart = e => handleOnDown(e.touches[0]);
 
 window.onmouseup = e => handleOnUp(e);
+
 window.ontouchend = e => handleOnUp(e.touches[0]);
 
 window.onmousemove = e => handleOnMove(e);
+
 window.ontouchmove = e => handleOnMove(e.touches[0]);
