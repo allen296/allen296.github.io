d708caefec51718d303eae030311f680544ac2c0
Antonio Valladares García <33860248+allen296@users.noreply.github.com>
2025-02-17 10:57:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,64 +1,41 @@
-const canvas = document.getElementById("backgroundCanvas");
-const ctx = canvas.getContext("2d");
+const track = document.getElementById("image-track");
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
 
-let particles = [];
+const handleOnUp = () => {
+    track.dataset.mouseDownAt = "0";  
+    track.dataset.prevPercentage = track.dataset.percentage;
+}
 
-class Particle {
-    constructor(x, y, size, speedX, speedY) {
-        this.x = x;
-        this.y = y;
-        this.size = size;
-        this.speedX = speedX;
-        this.speedY = speedY;
-    }
-
-    update() {
-        this.x += this.speedX;
-        this.y += this.speedY;
-
-        if (this.x > canvas.width || this.x < 0) this.speedX *= -1;
-        if (this.y > canvas.height || this.y < 0) this.speedY *= -1;
-    }
-
-    draw() {
-        ctx.fillStyle = "rgba(255, 255, 255, 0.8)";
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-        ctx.fill();
+const handleOnMove = e => {
+    if (track.dataset.mouseDownAt === "0") return;
+  
+    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+          maxDelta = window.innerWidth / 2;
+  
+    const percentage = (mouseDelta / maxDelta) * -100,
+          nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
+          nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+  
+    track.dataset.percentage = nextPercentage;
+  
+    track.animate({
+        transform: `translate(${nextPercentage}%, -50%)`
+    }, { duration: 1200, fill: "forwards" });
+  
+    for (const image of track.getElementsByClassName("image")) {
+        image.animate({
+            objectPosition: `${100 + nextPercentage}% center`
+        }, { duration: 1200, fill: "forwards" });
     }
 }
 
-function initParticles() {
-    particles = [];
-    for (let i = 0; i < 100; i++) {
-        let size = Math.random() * 5 + 1;
-        let x = Math.random() * canvas.width;
-        let y = Math.random() * canvas.height;
-        let speedX = (Math.random() - 0.5) * 2;
-        let speedY = (Math.random() - 0.5) * 2;
-        particles.push(new Particle(x, y, size, speedX, speedY));
-    }
-}
+/* Manejo de eventos para mouse y pantallas táctiles */
+window.onmousedown = e => handleOnDown(e);
+window.ontouchstart = e => handleOnDown(e.touches[0]);
 
-function animate() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+window.onmouseup = e => handleOnUp(e);
+window.ontouchend = e => handleOnUp(e.touches[0]);
 
-    particles.forEach(particle => {
-        particle.update();
-        particle.draw();
-    });
-
-    requestAnimationFrame(animate);
-}
-
-initParticles();
-animate();
-
-window.addEventListener("resize", () => {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    initParticles();
-});
+window.onmousemove = e => handleOnMove(e);
+window.ontouchmove = e => handleOnMove(e.touches[0]);
